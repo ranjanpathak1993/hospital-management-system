6ad5503dc45b49ef861cd3ecf4530908d67e2b4b
ranjanpathak1993 <ranjanpathak@zenexah.com>
2025-10-13 10:25:26 +0530

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hospital Management System</h1>
+      <p>Welcome to HMS frontend</p>
+    </div>
+  );
+}
+
+export default App;
